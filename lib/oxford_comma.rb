257cab0fe322ef6eb57f1523333e1432ded54e0d
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-17 02:57:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,7 @@ def oxford_comma(array)
     p array[0]
   elsif array.length == 2
     p array.join" and "
-  elsif array.length == 3
+  else array.length <= 3
     [array[0...-1].join(", "), array.last].join(", and ")
-  else array.length < 3
-    [array[0...-1].join(", "), array.last].join(", and ")
-
     end
-
 end
