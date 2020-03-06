982b32784221a1de96a45ca7b6264733411f775d
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-06 19:27:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++
@@ -1,9 +0,0 @@
-def oxford_comma(array)
-  if array.length == 1
-    p array[0]
-  elsif array.length == 2
-    p array.join" and "
-  else array.length <= 3
-    [array[0...-1].join(", "), array.last].join(", and ")
-    end
-end
